2ad26e7dfdd6c35b9ea8cf4e57dcae9c4087341d
Wytamma Wirth <wytamma.wirth@me.com>
2022-02-28 14:03:52 +1100

    :art: update bets templates

--- examples/BETS.sh
+++ examples/BETS.sh
@@ -8,19 +8,16 @@ for XML_FILE in $(ls examples/BETS-templates/*)
 do  
     GROUP_NAME="BETS/$(basename "${ALIGNMENT}" .fasta)/$(basename "${XML_FILE}" .xml)"
     pybeast \
-        --run sbatch \
         --overwrite \
-        --threads 8 \
-        --duplicates 3 \
+        --duplicates 1 \
         --template examples/slurm.template \
         -v cpus-per-task=8 \
         --group $GROUP_NAME \
         -d "alignment=$ALIGNMENT" \
         -d "Date.delimiter=_" \
         -d "Date.dateFormat=yyyy/M/dd" \
-        --ps \
-        -d "ps.nrOfSteps=50" \
-        -d "ps.chainLength=250000" \
-        -d "ps.rootdir={{run_directory}}/logs" \
+        --ns \
+        -d "mcmc.particleCount=1" \
+        -d "mcmc.threads=1" \
         $XML_FILE
 done
